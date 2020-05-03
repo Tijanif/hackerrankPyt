15f2dbcc2e955ff52f6167abe74daf57eb1f2959
Tijani Fulani <fulani07@gmail.com>
2020-05-03 14:19:42 +0100

    added if else

---
+++ Introduction/If_else.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    # If n is uneven return wierd
+    if (n % 2) == 1:
+        print('Weird')
+        # else return not wierd
+    else: 
+        print('Not Weird')
+
+
+# print hello 5 times
+if True:
+    print('Hello')
+    a = 5
