5530206600f83f34a78003e651181e84f26a4a94
Tijani Fulani <fulani07@gmail.com>
2020-05-03 14:26:58 +0100

    updated my if else

--- Introduction/If_else.py
+++ Introduction/If_else.py
@@ -1,12 +1,15 @@
 if __name__ == '__main__':
     n = int(input().strip())
-    # If n is uneven return wierd
-    if (n % 2) == 1:
-        print('Weird')
-        # else return not wierd
-    else: 
-        print('Not Weird')
-
+    # if n is not even print wierd
+if n % 2 != 0:
+    print ("Weird")
+else:
+    if n >= 2 and n <= 5:
+        print ("Not Weird")
+    elif n >= 6 and n <= 20:
+        print ("Weird")
+    elif n > 20:
+        print ("Not Weird")
 
 # print hello 5 times
 if True:
