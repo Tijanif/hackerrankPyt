edd5864724b2fc2de83f375d756230528c50483e
Tijani Fulani <fulani07@gmail.com>
2020-05-05 17:28:39 +0100

    converting floor

---
+++ Introduction/convert.py
@@ -0,0 +1,5 @@
+intput = input('European floor?') # what floor are you on in europe?
+usFloor = int(intput) + 1 # take the value of the european floor and add one to it and save to the us floor
+print('US floor', usFloor) # print the us floor value
+
+# European floor 0 = us floor 1 and so on
