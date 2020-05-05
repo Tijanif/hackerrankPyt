8997f94e3a2dd089264608919b6c184459f11dd3
Tijani Fulani <fulani07@gmail.com>
2020-05-05 17:43:37 +0100

    comparison operators

---
+++ Introduction/comparison operators.py
@@ -0,0 +1,12 @@
+x = 5 
+if x == 5:
+    print('Equals 5') # will print Equals 5
+if x > 4:
+    print('Greater than 4') # Greater than 4
+if x >= 5:
+    print('Greater than or Equals 5') # greater than or equals 5
+if x < 6: print('less than 6') # less than 6
+if x <= 5:
+    print('Less than or equal to 5') # less than or equal to 5
+if x != 6:
+    print('not equal to 6') #not equal to 6
